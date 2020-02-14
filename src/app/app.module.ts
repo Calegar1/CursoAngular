f8754a63e0ad803de9b07f0f94b8ed71aeb3840c
Calegari <fjcalegari@indracompany.com>
2020-02-14 11:42:47 -0300

    Menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,16 @@ import { FormsModule} from '@angular/forms';
 import { UsuarioFormComponent } from './usuario/usuario-form/usuario-form.component';
 import { UsuarioModule } from './usuario/usuario.module';
 import {HttpClientModule } from '@angular/common/http';
+import { MenuComponent } from './menu/menu.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PrimeiroComponent,
-    SegundoComponent
+    SegundoComponent,
+    MenuComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
