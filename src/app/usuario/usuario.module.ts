ebd60615ffe0d26638837d82267452e63c9f7575
Calegari <fjcalegari@indracompany.com>
2020-02-17 14:14:59 -0300

    furmulario completo

--- src/app/usuario/usuario.module.ts
+++ src/app/usuario/usuario.module.ts
@@ -7,11 +7,14 @@ import { DataPipe } from './pipes/data.pipe';
 import { UsuarioRoutingModule } from './usuario-routing.module';
 import { TemplateFormComponent } from './template-form/template-form.component';
 import { DataFormComponent } from './data-form/data-form.component';
+import { ShowErrosComponent } from './messages/show-erros/show-erros.component';
+import { DebugCampoComponent } from './messages/debug-campo/debug-campo.component';
+import { DebugFormComponent } from './messages/debug-form/debug-form.component';
 
 
 
 @NgModule({
-  declarations: [UsuarioFormComponent, GrifarParagrafoDirective, DataPipe, TemplateFormComponent, DataFormComponent, ],
+  declarations: [UsuarioFormComponent, GrifarParagrafoDirective, DataPipe, TemplateFormComponent, DataFormComponent, ShowErrosComponent, DebugCampoComponent, DebugFormComponent, ],
   imports: [
     CommonModule,
     UsuarioRoutingModule,
